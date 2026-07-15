90b64b46a665f6669757988dac9ed75b56192f3d
TechX-MD <puplicacount660@gmail.com>
2026-07-15 15:03:15 +0200

    Replace yt-dlp with music api

--- routes/play.js
+++ routes/play.js
@@ -1,75 +1,37 @@
 const express = require("express");
-const { exec } = require("child_process");
-const path = require("path");
-const fs = require("fs");
-const YoutubeSearchApi = require("youtube-search-api");
+const axios = require("axios");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (req,res)=>{
 
     const q = req.query.q;
 
-    if (!q) {
+    if(!q){
         return res.json({
-            success: false,
-            message: "Use ?q=song name"
+            success:false,
+            message:"Use ?q=song name"
         });
     }
 
     try {
 
-        const search = await YoutubeSearchApi.GetListByKeyword(
-            q,
-            false,
-            1
+        const response = await axios.get(
+            `https://api.popcat.xyz/ytmp3?url=${encodeURIComponent(q)}`
         );
 
-        if (!search.items.length) {
+        if(!response.data){
             return res.json({
                 success:false,
                 message:"Song not found"
             });
         }
 
-        const video = search.items[0];
-
-        const videoUrl = `https://www.youtube.com/watch?v=${video.id}`;
-
-        const fileName = Date.now() + ".mp3";
-        const output = path.join(
-            __dirname,
-            "../downloads",
-            fileName
-        );
-
-
-const command = `yt-dlp --js-runtimes deno --no-playlist -x --audio-format mp3 -o "${output}" "${videoUrl}"`;
-        exec(command, (error)=>{
-
-            if(error){
-                return res.json({
-                    success:false,
-                    error:error.message
-                });
-            }
-
-
-            if(!fs.existsSync(output)){
-                return res.json({
-                    success:false,
-                    message:"Audio not created"
-                });
-            }
-
-
-            res.json({
-                success:true,
-                title:video.title,
-                artist:"YouTube",
-                audio:`https://techx-api-79ow.onrender.com/downloads/${fileName}`
-            });
-
+        res.json({
+            success:true,
+            title:q,
+            artist:"Unknown",
+            audio:response.data.url
         });
 
 
