ac12691535663db518baa0d3fdc109618e9382d9
TechX-MD <puplicacount660@gmail.com>
2026-07-15 16:41:28 +0200

    Fix play route

--- routes/play.js
+++ routes/play.js
@@ -2,48 +2,73 @@ const express = require("express");
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+const yts = require("yt-search");
 
 const router = express.Router();
 
-router.get("/", async (req,res)=>{
+router.get("/", async (req, res) => {
 
     const q = req.query.q;
 
-    if(!q){
+    if (!q) {
         return res.json({
             success:false,
             message:"Use ?q=song name"
         });
     }
 
-    const fileName = Date.now() + ".mp3";
-    const output = path.join(__dirname,"../downloads",fileName);
+    try {
 
-const command = `yt-dlp --js-runtimes deno --extractor-args "youtube:player_client=android" --no-playlist -x --audio-format mp3 -o "${output}" "ytsearch1:${q}"`;
-    exec(command,(error)=>{
+        const result = await yts(q);
 
-        if(error){
+        const video = result.videos[0];
+
+        if (!video) {
             return res.json({
                 success:false,
-                error:error.message
+                message:"Song not found"
             });
         }
 
-        if(!fs.existsSync(output)){
-            return res.json({
-                success:false,
-                message:"Audio not created"
-            });
-        }
+        const fileName = Date.now() + ".mp3";
+        const output = path.join(__dirname, "../downloads", fileName);
 
-        res.json({
-            success:true,
-            title:q,
-            artist:"Unknown",
-            audio:`https://${req.get("host")}/downloads/${fileName}`
+        const command = `yt-dlp --js-runtimes deno --extractor-args "youtube:player_client=android_vr" --no-playlist -x --audio-format mp3 -o "${output}" "${video.url}"`;
+
+        exec(command, (error) => {
+
+            if (error) {
+                return res.json({
+                    success:false,
+                    error:error.message
+                });
+            }
+
+            if (!fs.existsSync(output)) {
+                return res.json({
+                    success:false,
+                    message:"Audio not created"
+                });
+            }
+
+            res.json({
+                success:true,
+                title:video.title,
+                artist:video.author.name,
+                thumbnail:video.thumbnail,
+                audio:`https://${req.get("host")}/downloads/${fileName}`
+            });
+
         });
 
-    });
+    } catch(err) {
+
+        res.json({
+            success:false,
+            error:err.message
+        });
+
+    }
 
 });
 
