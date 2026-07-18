f0a5bb84f0ba43c230c3fabf17ee43710cd288a6
TechX-MD <puplicacount660@gmail.com>
2026-07-18 09:57:48 +0200

    Replace ytdl with yt-dlp

--- routes/ytmp4.js
+++ routes/ytmp4.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const ytdl = require("@distube/ytdl-core");
-const fs = require("fs");
+const ytdlp = require("yt-dlp-exec");
 const path = require("path");
 
-router.get("/", async (req, res) => {
+router.get("/", async (req,res)=>{
 
     const url = req.query.url;
 
-    if (!url) {
+    if(!url){
         return res.json({
             success:false,
             message:"Use ?url=youtube_link"
@@ -17,29 +16,19 @@ router.get("/", async (req, res) => {
 
     try {
 
-        const info = await ytdl.getInfo(url);
-
-        const title = info.videoDetails.title
-        .replace(/[^a-zA-Z0-9]/g,"_");
-
         const file =
         path.join(
             __dirname,
-            "../downloads",
-            title + ".mp4"
+            "../downloads/video.mp4"
         );
 
-
-
-ytdl(url, {
-    quality:"highest",
-    filter:"audioandvideo"
-})        .pipe(fs.createWriteStream(file))
-        .on("finish",()=>{
-
-            res.download(file, title+".mp4");
-
+        await ytdlp(url, {
+            output: file,
+            format: "mp4"
         });
+
+
+        res.download(file);
 
 
     } catch(err){
@@ -54,6 +43,4 @@ ytdl(url, {
 });
 
 module.exports = router;
-
-
 
