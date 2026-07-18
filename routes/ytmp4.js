183b61719d9eeed5875308aa51a61ce7c6c4a521
TechX-MD <puplicacount660@gmail.com>
2026-07-18 10:03:44 +0200

    Use yt-dlp for ytmp4

--- routes/ytmp4.js
+++ routes/ytmp4.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const ytdlp = require("yt-dlp-exec");
+const { exec } = require("child_process");
 const path = require("path");
 
-router.get("/", async (req,res)=>{
+router.get("/", (req,res)=>{
 
     const url = req.query.url;
 
@@ -14,33 +14,29 @@ router.get("/", async (req,res)=>{
         });
     }
 
-    try {
+    const file = path.join(
+        __dirname,
+        "../downloads/video.mp4"
+    );
 
-        const file =
-        path.join(
-            __dirname,
-            "../downloads/video.mp4"
-        );
+    const command =
+    `yt-dlp -f "mp4" -o "${file}" "${url}"`;
 
-        await ytdlp(url, {
-            output: file,
-            format: "mp4"
-        });
+
+    exec(command, (error)=>{
+
+        if(error){
+            return res.json({
+                success:false,
+                error:error.message
+            });
+        }
 
 
         res.download(file);
 
-
-    } catch(err){
-
-        res.json({
-            success:false,
-            error:err.message
-        });
-
-    }
+    });
 
 });
 
 module.exports = router;
-
